7872c07f9610e1db3a7114c8af0e865ef084f3bb
Martin Croft <30621653+martincroft@users.noreply.github.com>
2019-09-25 13:47:16 +0100

    Docker uploads

---
+++ Docker Environment/Cleardown.bat
@@ -0,0 +1,8 @@
+docker stop edinburgh
+docker rm edinburgh
+docker stop manchester
+docker rm manchester
+docker stop glasgow
+docker rm glasgow
+cls
+docker ps -a
---
+++ Docker Environment/restart.bat
@@ -0,0 +1,6 @@
+docker stop edinburgh
+docker stop glasgow
+docker stop manchester
+docker start edinburgh
+docker start glasgow
+docker start manchester
